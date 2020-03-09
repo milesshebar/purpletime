53f33464c21e3c36866681cb060dbbd2d3784333
Glitch (hello-express) <none>
2020-03-09 20:55:47 +0000

    💋🧀 Checkpoint ./server.js:541641/2422

--- server.js
+++ server.js
@@ -1,34 +1,29 @@
-// server.js
-// where your node app starts
-
 // init project
 const express = require("express");
-var parseString = require('xml2js').parseString;
+var parseString = require("xml2js").parseString;
 const request = require("request");
 const tj = require("@tmcw/togeojson");
 const fs = require("fs");
 const DOMParser = require("xmldom").DOMParser;
 
-
-
 const options = {
-  method: 'GET',
-  uri: 'http://knox.ecolane.com/mde.php?q=vehicle_live'
-}
+  method: "GET",
+  uri: "http://knox.ecolane.com/mde.php?q=vehicle_live"
+};
 
 var location = request(options, function(err, res, body) {
-  if (err) { return console.log(err); }
-  var xml = body;  
-  parseString(xml, function (err, result) {
-            console.log(result.kml.Document[0].Placemark[10].name[0]);
-            console.log(result.kml.Document[0].Placemark[10].Point[0].coordinates[0]);
-        return result.kml.Document[0].Placemark[10].Point[0].coordinates[0];
+  if (err) {
+    return console.log(err);
+  }
+  var xml = body;
+  parseString(xml, function(err, result) {
+    
+    console.log(result.kml.Document[0].Placemark[10].name[0]);
+    console.log(result.kml.Document[0].Placemark[10].Point[0].coordinates[0]);
+    return result.kml.Document[0].Placemark[10].Point[0].coordinates[0];
     const json = JSON.stringify(result);
+  });
 });
-
-});
-
-
 
 const app = express();
 
