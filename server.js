9d1f5691b4db4922072fe7af9df1da7f3a06c4b8
Glitch (hello-express) <none>
2020-03-26 17:12:49 +0000

    🍅🏚 Checkpoint ./server.js:541641/428 ./.glitch-assets:541641/359

--- server.js
+++ server.js
@@ -19,9 +19,10 @@ var location = request(options, function(err, res, body) {
   var xml = body;
   
   parseString(xml, function(err, result) {
+    console.log(result.kml.Document[0].Placemark[5].name[0]);
+    console.log(result.kml.Document[0].Placemark[5].Point[0].coordinates[0]);
     const json = JSON.stringify(result);
-    console.log(result.kml.Document[0].Placemark[10].name[0]);
-    console.log(result.kml.Document[0].Placemark[10].Point[0].coordinates[0]);
+    
     return result.kml.Document[0].Placemark[10].Point[0].coordinates[0];
   });
 });
