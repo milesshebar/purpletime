7ed49e63d8edb6535a45dc306c978aa0396c92da
Glitch (hello-express) <none>
2020-03-26 23:29:02 +0000

    🌊👔 Checkpoint ./server.js:541641/6757

--- server.js
+++ server.js
@@ -4,44 +4,37 @@ var parseString = require("xml2js").parseString;
 const request = require("request");
 const tj = require("@tmcw/togeojson");
 const fs = require("fs");
-const util = require('util')
+const util = require("util");
 const DOMParser = require("xmldom").DOMParser;
 
 const options = {
   method: "GET",
-  uri: "https://knox.ecolane.com/mde.php?q=vehicle_live",
+  uri: "https://knox.ecolane.com/mde.php?q=vehicle_live"
 };
 
+const app = express();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
 
-var KATkml = request(options, function(err, res, body) {
+var KATkml = request(options, function putTemp(err, res, body) {
   if (err) {
     return console.log(err);
   }
   var kml = body;
-  fs.writeFile(__dirname + "/public/tmp.kml", kml, (err) => {
+  fs.writeFile(__dirname + "/public/tmp.kml", kml, err => {
     // throws an error, you could also catch it here
     if (err) throw err;
 
     // success case, the file was saved
-    console.log('saved!');
-});
-  parseString(kml, function(err, result) {
-    
-    console.log(result.kml.Document[0].Placemark[16].Point[0].coordinates[0]);
-    return result.kml.Document[0];
+    console.log("saved!");
   });
 });
 
+ setInterval(KATkml, 10000);
 
-
-const app = express();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
-
-io.on('connection', function (socket) {
-  socket.emit('kml', { kml: KATkml });
+io.on("connection", function(socket) {
+  socket.emit("kml", { kml: KATkml });
 });
-
 
 app.use(express.static("public"));
 
@@ -51,14 +44,13 @@ app.get("/", function(request, response) {
 
 // Send current time to all connected clients
 function sendTime() {
-    io.emit('kml', { kml: KATkml });
-    console.log("emitting");
+  io.emit("kml", { kml: KATkml });
+  console.log("emitting");
 }
 
 // Send current time every 10 secs
-setInterval(sendTime, 20000);
+setInterval(sendTime, 10000);
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(3000, function() {
+  console.log("listening on *:3000");
 });
-
