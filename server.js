4ebbe009276a3cd98b016926e15b8a36d649abe6
Glitch (hello-express) <none>
2020-04-29 00:48:48 +0000

    🎍💽 Checkpoint ./server.js:541641/1286

--- server.js
+++ server.js
@@ -27,22 +27,23 @@ function KATkml() {
     parseString(xml,function(err, result) {
       var jsoniem = JSON.stringify(result);
       result.kml.Document[0].Placemark.forEach(function (el) {
-        if (moment().isAfter(moment({ hour:20, minute: 0 }))) {
+       if (moment().isAfter(moment({ hour:14, minute: 0 }))) {
             if ( el.name == '124 (MTV-Gamb Evening)') {
               var data = el.Point[0].coordinates[0];
               var split = data.split(",");
               loc = split;
+              console.log(split);
               io.emit("shuttle", split);
             }       
-        }
+       }
 
-        else if ( el.name == '143 (MTV-Gamb Day)') {
+       /* else if ( el.name == '143 (MTV-Gamb Day)') {
           var data = el.Point[0].coordinates[0];
           var split = data.split(",");
           loc = split;
 
           io.emit("shuttle", split);
-        }      
+        }   */   
       });
     });
   })};
