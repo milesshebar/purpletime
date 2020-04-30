b07ab0f47580bbdbe72c2d9cc331a8c6819674c3
Glitch (hello-express) <none>
2020-04-30 17:27:51 +0000

    📷🍀 Checkpoint ./views/index.html:541641/8635 ./server.js:541641/5162

--- server.js
+++ server.js
@@ -2,57 +2,58 @@ const express = require("express");
 const request = require("request");
 const fs = require("fs");
 var parseString = require("xml2js").parseString;
-var moment = require('moment-timezone');
-moment().tz("America/Toronto").format();
+var moment = require("moment-timezone");
 
+//console.log("Currrent timezone: "); console.log(moment().tz());
+//console.log("Current time: "); console.log(moment().format());
+//console.log("Updating timezone: ");
+moment.tz.setDefault("America/New_York");
+//console.log("Current time: "); console.log(moment().format());
+//console.log("Currrent timezone after updating: "); console.log(moment().tz());
 
 const options = {
   method: "GET",
   uri: process.env.KAT_FEED
 };
 
-
 const app = express();
 var http = require("http").createServer(app);
 var io = require("socket.io")(http);
 
-var loc = '';
+var loc = "";
 
 function KATkml() {
   request(options, function(err, res, body) {
     if (err) throw err;
     var xml = body;
 
-    parseString(xml,function(err, result) {
+    parseString(xml, function(err, result) {
       var jsoniem = JSON.stringify(result);
-      var divider =  moment({ hour:14, minute: 0 });
-      var after = moment().isAfter(divider);
-      
-      console.log("after " + after);
-      
-      result.kml.Document[0].Placemark.forEach(function (el) {
-       if (!after) {
-        if ( el.name == '124 (MTV-Gamb Evening)') {
-          var data = el.Point[0].coordinates[0];
-          var split = data.split(",");
-          loc = split;
-          console.log(split);
-          io.emit("shuttle", split);
-        }       
-       }
-        
-       if ( el.name == '143 (MTV-Gamb Day)') {
-          var data = el.Point[0].coordinates[0];
-          var split = data.split(",");
-          loc = split;
+      var divider = moment({ hour: 14, minute: 0 });
 
+      var after = moment().isAfter(divider);
+
+      console.log("after " + after);
+
+      result.kml.Document[0].Placemark.forEach(function(el) {
+        if (after && el.name == "124 (MTV-Gamb Evening)") {
+          var data = el.Point[0].coordinates[0];
+          var split = data.split(",");
+          loc = split;
           io.emit("shuttle", split);
-        }      
+        }
+
+        if (!after && el.name == "143 (MTV-Gamb Day)") {
+          var data = el.Point[0].coordinates[0];
+          var split = data.split(",");
+          loc = split;
+          console.log(moment());
+          io.emit("shuttle", split);
+        }
       });
     });
-  })};
-
-
+  });
+}
 
 /*function KATkml() {
   request(options, function putTemp(err, res, body) {
@@ -69,11 +70,11 @@ function KATkml() {
 
 setInterval(KATkml, 10000);
 
-io.on('connect', onConnect);
+io.on("connect", onConnect);
 
-function onConnect(socket){
+function onConnect(socket) {
   socket.emit("init", loc);
-};
+}
 
 app.use(express.static("public"));
 
