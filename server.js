2bea94ec5ef87c9c90fbe9cd43508b60257415bb
Glitch (hello-express) <none>
2020-05-01 02:50:39 +0000

    🚆🎑 Checkpoint ./server.js:541641/27

--- server.js
+++ server.js
@@ -23,8 +23,6 @@ function KATkml() {
     parseString(xml, function(err, result) {
       var divider = moment({ hour: 14, minute: 0 });
       var after = moment().isAfter(divider);
-
-      console.log(after);
       
       result.kml.Document[0].Placemark.forEach(function(el) {
         if (after && el.name == "124 (MTV-Gamb Evening)") {
